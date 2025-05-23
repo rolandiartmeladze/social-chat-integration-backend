e9a89b09d894310ac253f3ea7308700ce5478197
Roland Artmeladze <rartmeladze@gmail.com>
2025-05-23 08:56:06 -0700

    feat: add GET endpoint for retrieving messages in Messenger routes

--- src/routes/messenger.ts
+++ src/routes/messenger.ts
@@ -6,5 +6,7 @@ const router = Router();
 
 router.get("/webhook", MessengerController.verifyWebhook);
 router.post("/webhook", MessengerController.recieveWebhook);
+router.get("/messages", MessengerController.getMessages);
+
 
 export default router;
