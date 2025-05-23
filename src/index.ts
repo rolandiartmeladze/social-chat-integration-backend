faf383032130f05fcc45ed20c5f6d44778dd8168
Roland Artmeladze <rartmeladze@gmail.com>
2025-05-23 04:01:41 -0700

    refactor: reorganize middleware and route setup in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+import messengerRouter from "./routes/messenger";
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+
+app.use("/messenger", messengerRouter);
+
+app.get("/", (_req, res) => {
   res.send("გამარჯობა Glitch-დან! This is a TypeScript + Node.js app.");
 });
 
