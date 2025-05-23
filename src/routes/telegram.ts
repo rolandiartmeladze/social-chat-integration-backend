f14e022d9ca1c822d3ef951080748779448a87f3
Roland Artmeladze <rartmeladze@gmail.com>
2025-05-23 14:15:48 -0700

    feat: add GET route for retrieving messages in Telegram controller

--- src/routes/telegram.ts
+++ src/routes/telegram.ts
@@ -4,5 +4,6 @@ import TelegramController from '../controllers/telegramController';
 const router = Router();
 
 router.post('/', TelegramController.receiveWebhook);
+router.get('/messages', TelegramController.getMessages);
 
 export default router;
