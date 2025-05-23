2da00f3adc5072d0d4c7510bca9486cf2b8cf4a4
Rolandi Artmeladze <rartmeladze@gmail.com>
2025-05-23 12:59:04 +0400

    update envirement

--- dev.ts
+++
@@ -1,8 +0,0 @@
-import { app } from './api/index';
-import http from 'http';
-
-const PORT = process.env.PORT || 5000;
-
-http.createServer(app).listen(PORT, () => {
-  console.log(`✅ Server running at http://localhost:${PORT}`);
-});
