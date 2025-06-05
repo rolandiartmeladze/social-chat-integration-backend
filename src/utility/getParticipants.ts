1051e3c5e0ef501d866aee666b7528bad0839efc
Roland Artmeladze <rartmeladze@gmail.com>
2025-06-05 11:04:56 -0700

    feat: add getParticipants utility to retrieve user and page details from participants

---
+++ src/utility/getParticipants.ts
@@ -0,0 +1,28 @@
+import { getUserAvatar } from "./getUserAvatar";
+import { User, Participants } from "../types/types"
+
+export async function getParticipants(
+    participantsRaw: any[],
+    pageId: string,
+    accessToken: string
+): Promise<Participants> {
+    const UnknownUser: User = { id: "", name: "Unknown", avatarUrl: "" };
+    const pageParticipantRaw = participantsRaw.find((p) => p.id === pageId);
+    const userParticipantRaw = participantsRaw.find((p) => p.id !== pageId);
+
+    const user: User = userParticipantRaw ? 
+        {
+            id: userParticipantRaw.id,
+            name: userParticipantRaw.name || "Unknown",
+            avatarUrl: await getUserAvatar(userParticipantRaw.id, accessToken) || "",
+        } : UnknownUser;
+
+    const page: User = pageParticipantRaw ?
+        {
+            id: pageParticipantRaw.id,
+            name: pageParticipantRaw.name || "Unknown",
+            avatarUrl: await getUserAvatar(pageParticipantRaw.id, accessToken) || "",
+        } : UnknownUser;
+
+    return { user, page };
+}
