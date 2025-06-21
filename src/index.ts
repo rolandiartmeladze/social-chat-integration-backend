914a22d04a4ea01551c30678e3737288c776e944
Roland Artmeladze <rartmeladze@gmail.com>
2025-06-21 06:08:49 -0700

    ref/ type detail import facebook auth component

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import MongoStore from "connect-mongo";
 import './auth/googleAuth'; // [REVIEW] Google OAuth-ის კონფიგურაცია, ინიციალიზაცია მოდულურად გამოყოფილია, რაც კარგია.
+import './auth/facebookAuth'; 
 
 import { createServer } from "http";
 import { initSocket } from "./socket";
